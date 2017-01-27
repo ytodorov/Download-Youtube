73f119541311e6a5a60498caef4bd4aa21b3fe14
ytodorov <ytodorov@ytodorov.com>
2017-01-27 11:49:23 +0200

    remove libraries

--- DownloadYoutubeWeb/DownloadYoutubeWeb/Infrastructure/AudioUtils.cs
+++ DownloadYoutubeWeb/DownloadYoutubeWeb/Infrastructure/AudioUtils.cs
@@ -1,5 +1,4 @@
-﻿using NAudio.Wave;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,22 +9,6 @@ namespace DownloadYoutubeWeb.Infrastructure
 {
     public static class AudioUtils
     {
-        public static void ConvertAiffToWav(string aiffFile, string wavFile)
-        {
-            using (AiffFileReader reader = new AiffFileReader(aiffFile))
-            {
-                using (WaveFileWriter writer = new WaveFileWriter(wavFile, reader.WaveFormat))
-                {
-                    byte[] buffer = new byte[4096];
-                    int bytesRead = 0;
-                    do
-                    {
-                        bytesRead = reader.Read(buffer, 0, buffer.Length);
-                        writer.Write(buffer, 0, bytesRead);
-                    } while (bytesRead > 0);
-                }
-            }
-        }
 
         public static byte[] GetMp3Bytes(byte[] inputBytes, string fuleName)
         {
